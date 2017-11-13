9a90f8c9f441d88f04c1dd7b5b840baf27c6d3bd
Joen Tolgraven <joen.tolgraven@gmail.com>
2017-11-13 20:10:15 +0100

    all bus stuff commented out, no go, prob hw issue

--- src/artnet_d1.cpp
+++ src/artnet_d1.cpp
@@ -47,8 +47,8 @@ uint16_t led_count;
 WiFiUDP udp;
 ArtnetnodeWifi artnet;
 // XXX how get around having to init RGB or GRBW hardcoded?
-NeoPixelBus<NeoGrbwFeature, NeoEsp8266BitBang800KbpsMethod> *bus = NULL;	// this way we can (re!)init off config values, post boot
-NeoPixelBus<NeoGrbwFeature, NeoEsp8266BitBang800KbpsMethod> *bus2 = NULL;	// this way we can (re!)init off config values, post boot
+// NeoPixelBus<NeoGrbwFeature, NeoEsp8266BitBang800KbpsMethod> *bus = NULL;	// this way we can (re!)init off config values, post boot
+// NeoPixelBus<NeoGrbwFeature, NeoEsp8266BitBang800KbpsMethod> *bus[2] = {};	// this way we can (re!)init off config values, post boot
 // use pointer array to be able to store several, I guess (for different pins/universes, if that's actually proper feasible)
 // WS2812FX ws2812fx = WS2812FX(LED_COUNT, LED_PIN, NEO_RGB + NEO_KHZ800);
 Timer timer;
@@ -61,18 +61,19 @@ void flushNeoPixelBus(uint16_t universe, uint16_t length, uint8_t sequence, uint
 
 void setup() {
 	Homie.disableResetTrigger(); Homie_setFirmware("artnet", "1.0.1");
-	cfg_strips.setDefaultValue(1); cfg_pin.setDefaultValue(LED_PIN); cfg_start_uni.setDefaultValue(1); cfg_start_addr.setDefaultValue(1);
+	cfg_strips.setDefaultValue(1); cfg_pins.setDefaultValue(LED_PIN); cfg_start_uni.setDefaultValue(1); cfg_start_addr.setDefaultValue(1);
 
 	Homie.setup();
+	Homie.getLogger() << "Homie inited, ";
 	bytes_per_pixel = cfg_bytes.get(); //led_count = cfg_count.get();
 	// universes = cfg_universes.get();
 		
-	if(bus) delete bus;
+	// if(bus) delete bus; // 'enabled by default' warning, so nuke
 	// XXX split cfg_pins and get from there... use array of pointers for buses
-	bus = new NeoPixelBus<NeoGrbwFeature, NeoEsp8266BitBang800KbpsMethod>(5);
-	bus+1 = new NeoPixelBus<NeoGrbwFeature, NeoEsp8266BitBang800KbpsMethod>(6);
-	bus->Begin();
-	bus+1->Begin();
+	// bus = new NeoPixelBus<NeoGrbwFeature, NeoEsp8266BitBang800KbpsMethod>(cfg_count.get(), 5);
+	// bus[1] = new NeoPixelBus<NeoGrbwFeature, NeoEsp8266BitBang800KbpsMethod>(cfg_count.get(), 6);
+	// bus->Begin();
+	// bus[1]->Begin();
 	
   artnet.setName(Homie.getConfiguration().name);
   artnet.setNumPorts(cfg_universes.get()); 	// wrong obvs since we have 1 strip, 2 unis.,..
@@ -85,13 +86,13 @@ void setup() {
 
 	setupOTA(); 
 
-	Homie.getLogger() << "All prepped n ready to break!!!" << endl;
+	Homie.getLogger() << "setup finished" << endl;
 }
 
 void logPixels() {
-	size_t bufsize = bus->PixelsSize();
-	uint8_t* pixels = bus->Pixels();
-	for(size_t p = 0; p < bufsize; ++p)  Serial.printf("%d ", pixels[p]);
+	// size_t bufsize = bus->PixelsSize();
+	// uint8_t* pixels = bus->Pixels();
+	// for(size_t p = 0; p < bufsize; ++p)  Serial.printf("%d ", pixels[p]);
 }
 
 
@@ -113,17 +114,19 @@ void flushNeoPixelBus(uint16_t universe, uint16_t len, uint8_t sequence, uint8_t
   for(uint16_t t = DMX_FUNCTION_CHANNELS; t < len; t += bytes_per_pixel) { // loop each pixel (4 bytes) in universe
 		if(bytes_per_pixel == 3) {
 			RgbColor color 	= RgbColor(data[t], data[t+1], data[t+2]);
-			bus[universe - 1]->SetPixelColor(pixel, color); // XXX not hardcode universe to start at 1 (map unis to buses, not straight index)
+			// bus[universe - 1]->SetPixelColor(pixel, color); // XXX not hardcode universe to start at 1 (map unis to buses, not straight index)
+			// bus->SetPixelColor(pixel, color); // XXX not hardcode universe to start at 1 (map unis to buses, not straight index)
 		} else if(bytes_per_pixel == 4) { 
 			RgbwColor color = RgbwColor(data[t], data[t+1], data[t+2], data[t+3]);
-			bus[universe - 1]->SetPixelColor(pixel, color);
+			// bus->SetPixelColor(pixel, color);
 		} pixel++;
 	}
 	if(functions[CH_DIMMER] != 255) {
 		// scale fucking everything. But also should implement above-100 gain that'd be cool... Maybe reg dimmer and then part of ch7 or 8 or whatever is extra gain? hmm
 
 	}
-	bus[universe - 1]->Show();
+	// bus[universe - 1]->Show();
+	// bus->Show();
 }
 
 void loop() {
