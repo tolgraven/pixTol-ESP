40cfbf621880d3518b9efc41db57f7a2994879b9
Joen Tolgraven <joen.tolgraven@gmail.com>
2018-05-09 00:32:39 +0300

    pio homie ota build script properly uploads to central fw update server, and call to python utility for pushing straight from terminal also works, tho errors out. probably solutions from the internet

--- build_scripts/ota.py
+++ build_scripts/ota.py
@@ -2,64 +2,64 @@ Import("env")
 from subprocess import call
 from base64 import b64decode
 from SCons.Script import ARGUMENTS, AlwaysBuild
-
 # Upload actions
-#
 # def before_upload(source, target, env):
 #     print "before_upload"
 # env.AddPreAction("upload", before_upload)
 
 # Custom actions when building program/firmware
-#
 # env.AddPreAction("buildprog", callback...)
 # env.AddPostAction("buildprog", callback...)
 # env.AddPostAction("size", env.VerboseAction(" ".join(["curl", 
-#    "-F", "upload=@$BUILD_DIR/firmware.bin", 
-#    "-F", "description='pixtol'", 
-#    "http://$OTA_HOST:9080/upload"
-#    ]), "Uploading $BUILD_DIR/firmware.bin using homie"))
+#    "-F", "upload=@$BUILD_DIR/firmware.bin", "-F", "description='pixtol'", 
+#    "http://$OTA_HOST:9080/upload" ]), "Uploading $BUILD_DIR/firmware.bin using homie"))
 
-# Custom actions for specific files/objects
-#
 def homie_ota_upload(source, target, env):
     host = b64decode(ARGUMENTS.get("OTA_HOST"))
+    unit = b64decode(ARGUMENTS.get("UNIT"))
+    print "\nUploading firmware.bin to ota server.\n"
     env.Execute("curl -F upload=@$BUILD_DIR/firmware.bin -F description='pixtol' http://" + host + ":9080/upload")
-    env.Execute("pkill pio")
+
+    #figure out what's wrong...
+    # print "\nFlashing firmware.bin over mqtt...\n"
+    # env.Execute("homie_ota_updater -t pixtol/ -i " + unit + " $BUILD_DIR/firmware.bin")
+    # env.Executej("pkill pio")
 
 env.AddPostAction("$BUILD_DIR/firmware.bin", homie_ota_upload)
 
 # custom action before building SPIFFS image. For example, compress HTML, etc.
 def spiffs_ota_homie(source, target, env):
     host = b64decode(ARGUMENTS.get("OTA_HOST"))
-    env.Execute("cp $BUILD_DIR/spiffs.bin $PROJECT_DIR/firmware/")
+    env.Execute("cp $BUILD_DIR/spiffs.bin $PROJECT_DIR/firmware/stash/")
     print "\nCopied spiffs.bin to fw dir.\n"
     # env.Execute("curl -F upload=@$PROJECT_DIR/firmware/spiffs.bin -F description='pixtol' http://" + host + ":9080/upload")
     # env.Execute("esptool.py lala")
 env.AddPostAction("$BUILD_DIR/spiffs.bin", spiffs_ota_homie)
 
-# custom action for project's main.cpp
-# env.AddPostAction("$BUILD_DIR/src/main.cpp.o", callback...)
-
-# Custom HEX from ELF
-# env.AddPostAction( "$BUILD_DIR/firmware.elf",
-#     env.VerboseAction(" ".join([
-#         "$OBJCOPY", "-O", "ihex", "-R", ".eeprom",
-#         "$BUILD_DIR/firmware.elf", "$BUILD_DIR/firmware.hex"
-#     ]), "Building $BUILD_DIR/firmware.hex")
-# )
-
-def runota_callback(*args, **kwargs):
-    print "Hello PlatformIO!"
+# def runota_callback(*args, **kwargs):
+def runota_callback(source, target, env):
     host = b64decode(ARGUMENTS.get("OTA_HOST"))
+    unit = b64decode(ARGUMENTS.get("UNIT"))
     # mode = b64decode(ARGUMENTS.get("OTA_MODE")) #arduino, homie-ota, homie2?
-    # print "OTA host: ", host
-    env.Execute("cp $BUILD_DIR/firmware.bin $PROJECT_DIR/firmware/")
-    env.Execute("curl -F upload=@$PROJECT_DIR/firmware/firmware.bin -F description='pixtol' http://" + host + ":9080/upload")
+    env.Execute("cp $BUILD_DIR/firmware.bin $PROJECT_DIR/firmware/stash/")
+    # env.Execute("curl -F upload=@$PROJECT_DIR/firmware/stash/firmware.bin -F description='pixtol' http://" + host + ":9080/upload")
+    # env.Execute("pkill esptool")
+    env.Execute("homie_ota_updater -t pixtol/ -i " + unit + "$BUILD_DIR/firmware.bin")
+
 AlwaysBuild(env.Alias("ota", "", runota_callback))
 
 def print_env(*args, **kwargs):
     print env.Dump()
 AlwaysBuild(env.Alias("env", "", print_env))
+
 def print_targets(*args, **kwargs):
     print "Current build targets", map(str, BUILD_TARGETS)
 AlwaysBuild(env.Alias("targets", "", print_targets))
+
+
+# env.AddPostAction("$BUILD_DIR/src/main.cpp.o", callback...)   # custom action for project's main.cpp
+
+# env.AddPostAction( "$BUILD_DIR/firmware.elf",     #Custom HEX from ELF
+#     env.VerboseAction(" ".join([ "$OBJCOPY", "-O", "ihex", "-R", ".eeprom",
+#         "$BUILD_DIR/firmware.elf", "$BUILD_DIR/firmware.hex" ]), "Building $BUILD_DIR/firmware.hex"))
+
