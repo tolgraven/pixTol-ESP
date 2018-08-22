66205b14812894c60b1c90a3ce686fe370a8ff85
Joen Tolgraven <joen.tolgraven@gmail.com>
2018-08-22 22:29:30 +0200

    update util

--- src/util.cpp
+++ src/util.cpp
@@ -1,91 +1,60 @@
 #include "util.h"
 
-const RgbwColor black =   RgbwColor(0, 0, 0, 0);
-const RgbwColor white =   RgbwColor(30, 30, 30, 80);
-const RgbwColor red =     RgbwColor(170, 25, 10, 20);
-const RgbwColor orange =  RgbwColor(150, 70, 10, 20);
-const RgbwColor yellow =  RgbwColor(150, 150, 20, 30);
-const RgbwColor green =   RgbwColor(20, 170, 40, 20);
-const RgbwColor blue =    RgbwColor(20, 60, 180, 20);
-const RgbColor blueZ =    RgbColor(25, 90, 220);
-
 const RgbwColor* otaColor;
 
 ArtnetnodeWifi artnet;
-WiFiUDP udp;
 
 HomieNode modeNode("mode", "mode");
+HomieNode statusNode("status", "log");
+HomieNode outputNode("outputs", "io", outputNodeHandler);
+HomieNode inputNode("inputs",   "io", inputNodeHandler);
 
-HomieNode outputNode("io", "outputs", outputHandler);
-HomieNode inputNode("io", "inputs", inputHandler);
 
-bool outputHandler(const String& property, const HomieRange& range, const String& value) {
+bool outputNodeHandler(const String& property, const HomieRange& range, const String& value) {
   outputNode.setProperty(property).send(value);
  	LN.logf(__PRETTY_FUNCTION__, LoggerNode::DEBUG, "Got prop %s, value=%s", property.c_str(), value.c_str());
 
-  if(property.equals("strip")) {
-    if(value.equals("on")) {
-      pinMode(RX, OUTPUT);
-    } else {
-      pinMode(RX, INPUT);
-    }
-  }
-  // else if(property.equals("serial")) {
-  //
-  // }
+  if(property == "strip") {
+    if(value == "on") pinMode(RX, OUTPUT);
+    else              pinMode(RX, INPUT_PULLUP);
+  } else if(property == "serial") {
+  } return true;
 }
 
-bool inputHandler(const String& property, const HomieRange& range, const String& value) {
+bool inputNodeHandler(const String& property, const HomieRange& range, const String& value) {
   inputNode.setProperty(property).send(value);
  	LN.logf(__PRETTY_FUNCTION__, LoggerNode::DEBUG, "Got prop %s, value=%s", property.c_str(), value.c_str());
 
-  if(property.equals("artnet")) {
-    if(value.equals("on")) {
-      artnet.enableDMX();
-      // for(uint8_t i=0; i < cfg->universes.get(); i++) {
-      //     artnet.enableDMXOutput(i);
-      // }
+  bool on = value == "on";
+  // future: for registered inputters; if property==inputter.name; pass the message...
+  if(property == "artnet") {
+    if(on)  artnet.enableDMX();
+    else    artnet.disableDMX();
+  } else if(property == "serial") {
+    if(on) { //configure serial port for data input
     } else {
-      artnet.disableDMX();
-      // for(uint8_t i=0; i < cfg->universes.get(); i++) {
-      //     artnet.disableDMXOutput(i);
-      // }
     }
-  // } else if(property.equals("serial")) {
-  //   if(value.equals("on")) { //configure serial port for data input
-  //
-  //   } else {
-  //
-  //   }
-  // } else if(property.equals("mqtt")) {
-  //   if(value.equals("on")) {
-  //
-  //   } else {
-  //
-  //   }
-  // } else if(property.equals("dmx")) {
-  //   if(value.equals("on")) {
-  //
-  //   } else {
-  //
-  //   }
-  }
-	return true;
+  } else if(property == "mqtt") {
+    if(on) {
+    } else {
+    }
+  } else if(property == "dmx") {
+    if(on) {
+    } else {
+    }
+  } return true;
 }
 
 
 // put in animation class for strip, or at least pass a bus object lol
 void blinkStrip(uint8_t numLeds, RgbwColor color, uint8_t blinks) {
-  DmaGRBW tempbus(numLeds); // want to clear any previous garbage as well...
-  tempbus.Begin(); tempbus.Show();
+  Strip tempbus("temp", LEDS::RGBW, numLeds);
   delay(100); // XXX use callbacks instead of delays, then mqtt-ota will prob work? Also move this sorta routine into Strip class, or some animation class?
   for(int8_t b = 0; b < blinks; b++) {
-    tempbus.ClearTo(color); tempbus.Show();
-    yield();
-    delay(50);
-    tempbus.ClearTo(black); tempbus.Show();
-    yield();
-    delay(50);
+    tempbus.SetColor(color);
+    yield(); delay(50);
+    tempbus.SetColor(colors.black);
+    yield(); delay(50);
   }
 }
 
@@ -93,15 +62,13 @@ void initArtnet(const String& name, uint8_t numPorts, uint8_t startingUniverse, 
   artnet.setName(name.c_str());
   artnet.setNumPorts(numPorts);
 	artnet.setStartingUniverse(startingUniverse);
-	for(uint8_t i=0; i < numPorts; i++) {
-      artnet.enableDMXOutput(i);
-  }
+	for(int i=0; i<numPorts; i++) artnet.enableDMXOutput(i);
 	artnet.begin();
 	artnet.setArtDmxCallback(callback);
 }
 
 
-DmaGRBW *OTAbus;
+Strip* OTAbus;
 uint16_t leds;
 int prevPixel = -1;
 
@@ -111,83 +78,74 @@ void setupOTA(uint8_t numLeds) {
 
   ArduinoOTA.onStart([]() {
     Serial.println("\nOTA flash starting...");
-    OTAbus = new NeoPixelBrightnessBus<NeoGrbwFeature, NeoEsp8266Dma800KbpsMethod>(leds);
-    OTAbus->Begin(); OTAbus->Show();
-    otaColor = (ArduinoOTA.getCommand() == U_FLASH ? &blue : &yellow);
+    OTAbus = new Strip("OTA strip", LEDS::RGBW, leds);
+    OTAbus->Show();
+    otaColor = (ArduinoOTA.getCommand() == U_FLASH ? &colors.blue : &colors.yellow);
   });
 
   ArduinoOTA.onProgress([](unsigned int p, unsigned int tot) {
     uint8_t pixel = p / (tot / leds);
-    if(pixel == prevPixel) return; // called multiple times each percent of upload...
-    if(pixel) {
-      RgbwColor prevPixelColor = OTAbus->GetPixelColor(prevPixel);
-      prevPixelColor.Darken(80);
-      OTAbus->SetPixelColor(prevPixel, prevPixelColor);
+    if(pixel == prevPixel) { // called multiple times each percent of upload...
+      // otaColor->Lighten(10);
+      return;
+    } else {
+      // otaColor = (ArduinoOTA.getCommand() == U_FLASH ? &colors.blue : &colors.yellow);
     }
-    OTAbus->SetPixelColor(pixel, *otaColor);
+    if(pixel) {
+      RgbwColor prevPixelColor;
+      OTAbus->driver->GetPixelColor(prevPixel, prevPixelColor);
+      prevPixelColor.Darken(25);
+      OTAbus->driver->SetPixelColor(prevPixel, prevPixelColor);
+    }
+    OTAbus->driver->SetPixelColor(pixel, *otaColor);
     OTAbus->Show();
     prevPixel = pixel;
-    Serial.printf("OTA updating - %u%%\r", p / (tot/100));
-  });
+    Serial.printf("OTA updating - %u%%\r", p / (tot/100)); });
 
-  ArduinoOTA.onEnd([]() {
-    blinkStrip(leds, green, 3);
-  });
+
+  ArduinoOTA.onEnd([]() { blinkStrip(leds, colors.green, 3); });
 
   ArduinoOTA.onError([](ota_error_t error) {
     Serial.printf("OTA Error[%u]: ", error);
-    switch(error) { 
+    switch(error) {
       case OTA_AUTH_ERROR:		Serial.println("Auth Failed"); 		break;
       case OTA_BEGIN_ERROR:		Serial.println("Begin Failed");		break;
       case OTA_CONNECT_ERROR:	Serial.println("Connect Failed");	break;
       case OTA_RECEIVE_ERROR:	Serial.println("Receive Failed");	break;
       case OTA_END_ERROR:			Serial.println("End Failed");			break;
-	  }
-    blinkStrip(leds, red, 3);
-  });
+	  } blinkStrip(leds, colors.red, 3); });
 
   ArduinoOTA.begin();
 }
 
 
 bool globalInputHandler(const HomieNode& node, const String& property, const HomieRange& range, const String& value) {
-  // Homie.getLogger() << "Received on node " << node.getId() << ": " << property << " = " << value << endl;
   LN.logf(node.getId(), LoggerNode::INFO, "%s: %s", property.c_str(), value.c_str());
 
-  if(property.equals("command")) {
-		if(value.equals("reset")) Homie.reset();
-		if(value.equals("restart")) ESP.restart();
-		else if(value.equals("ledoff")) Homie.disableLedFeedback();
-	}
-  else if(property.equals("mode")) {
-		if(value.equals("dmx")) {
+  if(property == "command") {
+		if(value == "reset")        Homie.reset();
+		if(value == "restart")      ESP.restart();
+		else if(value == "ledoff")  Homie.disableLedFeedback();
+	} else if(property == "mode") {
+		if(value == "dmx") {
 			// eventually multiple input/output types etc, LTP/HTP/average/prio, specific mergin like
 			// animation over serial, DMX FN CH from artnet
 			// animation over artnet, fn ch overrides from mqtt
-
-		} else if(value.equals("mqtt")) {
-
-		} else if(value.equals("standalone")) {
-
+		} else if(value == "mqtt") {
+		} else if(value == "standalone") {
 		}
-  } else {
-    return false;
-  }
+  } else return false;
   return true;
 }
 
-bool broadcastHandler(const String& level, const String& value) {
-  // not sure what to use for but will prob get something...
+bool broadcastHandler(const String& level, const String& value) { // not sure what to use for but will prob get something...
   LN.logf("BROADCAST", LoggerNode::INFO, "Level %s: %s", level.c_str(), value.c_str());
-  // Serial << "Received broadcast level " << level << ": " << value << endl;
   return true;
 }
 
 void onHomieEvent(const HomieEvent& event) {
   switch(event.type) {
-    case HomieEventType::STANDALONE_MODE: {
-      break;
-    }
+    case HomieEventType::STANDALONE_MODE: { break; }
     case HomieEventType::CONFIGURATION_MODE: // blink orange or something?
       // blinkStrip(leds, orange, 5);
       break;
@@ -197,40 +155,13 @@ void onHomieEvent(const HomieEvent& event) {
     case HomieEventType::OTA_STARTED: {
       Serial.println("\nOTA flash starting...");
       LN.logf("OTA", LoggerNode::INFO, "Ota ON OTA ON!!!");
-      artnet.disableDMX();
-      // kill all inputs and outputs, etcetc
-
-      // if(buses[0].bus) {
-      //     buses[0].bus->ClearTo(blueZ);
-      // yield();
-      //     buses[0].bus->Show();
-      // }
-      // if(buses[0].busW) {
-      //     buses[0].busW->ClearTo(blue);
-      // yield();
-      //     buses[0].busW->Show();
-      // }
-
-      // OTAbus = new NeoPixelBrightnessBus<NeoGrbwFeature, NeoEsp8266Dma800KbpsMethod>(5);
-      // OTAbus->Begin();
-      // OTAbus->Show();
-      // otaColor = &blue;
+      artnet.disableDMX(); // kill all inputs and outputs, etcetc
       break;
     }
     case HomieEventType::OTA_PROGRESS: { // can use event.sizeDone and event.sizeTotal
       uint8_t pixel = event.sizeDone / (event.sizeTotal / leds);
-      if(pixel == prevPixel) break; // called multiple times each percent of upload...
-    //   if(pixel) {
-    //     RgbwColor prevPixelColor = OTAbus->GetPixelColor(prevPixel);
-    //     prevPixelColor.Darken(80);
-    //     OTAbus->SetPixelColor(prevPixel, prevPixelColor);
-    //   }
-    //   OTAbus->SetPixelColor(pixel, *otaColor);
-    //   OTAbus->Show();
-    //   prevPixel = pixel;
+      // if(pixel == prevPixel) break; // called multiple times each percent of upload...
       Serial.printf("OTA updating - %u%%\r", event.sizeDone / (event.sizeTotal / leds));
-    //   // buses[0].busW->SetPixelColor(event.sizeDone / (event.sizeTotal / leds), blue);
-    //   // buses[0].busW->Show();
       break;
     }
     case HomieEventType::OTA_FAILED: {
@@ -244,28 +175,21 @@ void onHomieEvent(const HomieEvent& event) {
       Homie.setIdle(false);
       LN.logf("OTA", LoggerNode::INFO, "OTA GOODE");
       // blinkStrip(leds, green, 1);
-      // yield();
-      // delay(50);
       Homie.setIdle(true);
       break;
     }
-    case HomieEventType::ABOUT_TO_RESET:
-      // blinkStrip(leds, orange, 0);
+    case HomieEventType::ABOUT_TO_RESET: // blinkStrip(leds, orange, 0);
       break;
-    case HomieEventType::WIFI_CONNECTED: // normal mode, can use event.ip, event.gateway, event.mask
+    case HomieEventType::WIFI_CONNECTED:
       // blinkStrip(leds, green, 1);
-      break;
-    case HomieEventType::WIFI_DISCONNECTED: // normal mode, can use event.wifiReason
+      break; // normal mode, can use event.ip, event.gateway, event.mask
+    case HomieEventType::WIFI_DISCONNECTED:
       // blinkStrip(leds, red, 0);
-      break;
-    case HomieEventType::MQTT_READY:
-      break;
-    case HomieEventType::MQTT_DISCONNECTED: // can use event.mqttReason
-      break;
-    case HomieEventType::MQTT_PACKET_ACKNOWLEDGED: // MQTT packet with QoS > 0 is acknowledged by the broker, can use event.packetId
-      break;
-    case HomieEventType::READY_TO_SLEEP: // After calling prepareToSleep() the event is triggered when MQTT is disconnected
-      break;
+      break; // normal mode, can use event.wifiReason
+    case HomieEventType::MQTT_READY: break;
+    case HomieEventType::MQTT_DISCONNECTED: break; // can use event.mqttReason
+    case HomieEventType::MQTT_PACKET_ACKNOWLEDGED: break; // MQTT packet with QoS > 0 is acknowledged by the broker, can use event.packetId
+    case HomieEventType::READY_TO_SLEEP: break; // After calling prepareToSleep() the event is triggered when MQTT is disconnected
   }
 }
 
--- src/util.h
+++ src/util.h
@@ -1,36 +1,39 @@
 #pragma once
 
-#include <NeoPixelBrightnessBus.h>
-#include <NeoPixelAnimator.h>
 #include <Homie.h>
 #include <LoggerNode.h>
 #include <ArtnetnodeWifi.h>
-#include <WiFiUdp.h>
 #include <Ticker.h>
 #include <ArduinoOTA.h>
-#include "strip.h"
+
+#include "io/strip.h"
 #include "config.h"
 
-const extern RgbwColor black, white, red, orange, yellow, green, blue;
-const extern RgbColor blueZ;
+struct Colors {
+  RgbwColor black =   RgbwColor(0, 0, 0, 0);
+  RgbwColor white =   RgbwColor(50, 50, 50, 150);
+  RgbwColor red =     RgbwColor(190, 30, 15, 25);
+  RgbwColor orange =  RgbwColor(170, 85, 12, 28);
+  RgbwColor yellow =  RgbwColor(160, 160, 20, 35);
+  RgbwColor green =   RgbwColor(30, 190, 45, 25);
+  RgbwColor blue =    RgbwColor(30, 70, 190, 25);
+  RgbColor  blueZ =   RgbColor(25, 90, 220);
+}; const Colors colors;
 
 extern HomieNode modeNode;
-
+extern HomieNode statusNode;
 extern HomieNode outputNode;
 extern HomieNode inputNode;
 
-
 extern ArtnetnodeWifi artnet;
-extern WiFiUDP udp;
 
-bool outputHandler(const String& property, const HomieRange& range, const String& value);
-bool inputHandler(const String& property, const HomieRange& range, const String& value);
+bool outputNodeHandler(const String& property, const HomieRange& range, const String& value);
+bool inputNodeHandler(const String& property, const HomieRange& range, const String& value);
 void initArtnet(const String& name, uint8_t numPorts, uint8_t startingUniverse, void (*callback)(uint16_t, uint16_t, uint8_t, uint8_t*));
 
 void blinkStrip(uint8_t numLeds, RgbwColor color, uint8_t blinks);
 void blinkStatus(RgbColor color, uint8_t blinks);
 void setupOTA(uint8_t numLeds);
-void logIfChanged(int8_t level, std::string topic, std::string msg);
 extern bool globalInputHandler(const HomieNode& node, const String& property, const HomieRange& range, const String& value);
 extern bool broadcastHandler(const String& level, const String& value);
 extern void onHomieEvent(const HomieEvent& event); //extern since is passed rather than called in artnet_d1,cpp
