8a40761e6b68001eead51254d40335b9cf72c42a
Joen Tolgraven <joen.tolgraven@gmail.com>
2018-12-14 12:56:51 +0300

    overall layout of everything-as-config-data set. not remotely functioning

--- src/config.h
+++ src/config.h
@@ -105,3 +105,184 @@ class ConfigNode: public HomieNode { // seems homie (now?) has a 10 settings lim
   //   // advertise("debug").settable(); //don't need this cause can update settings ocer mqtt anyways
   // }
 };
+
+struct PixtolConfig {
+  PixtolConfig(const String& id): id(id) {}
+  String type, id; //name of app/thing, id of individual unit
+
+  struct Port {
+    Port(const String& id, uint16_t port): id(id), port(port) {}
+    String id;
+    uint16_t port; //universe or equiv, network port, physical pin/port id, w/e
+    // should this contain kind, or is that for owner to know?
+    // prob go in here somehow? but like from above types, not "artnet" or w/e anyways bc we also want
+    // bool exclusive; //do we require exclusive access? yes for network binds, physical... wait, everything, nvm
+  };
+  struct TransformConfig {
+    bool mirror = false, fold = false, flip = false; //for now. look in NPB geometry for insp...
+  };
+  enum DataUnit { BYTE, DOUBLEBYTE, FLOAT };
+  struct IOConfig {
+    IOConfig(const String& id, uint8_t fieldSize, uint16_t fieldCount, DataUnit dataType = BYTE):
+      id(id), fieldSize(fieldSize), fieldCount(fieldCount), dataType(dataType) {
+      }
+    IOConfig& addPort(const String& id, uint16_t number) {
+      ports.push_back(Port(id, number));
+      return *this;
+    }
+    String id; //eg "ArtNet". use to look up from list of available...
+    uint8_t fieldSize;
+    uint16_t fieldCount;
+    DataUnit fieldType; // uint16_t size; //size, in bytes, required for each port buffer
+    std::vector<Port> ports;
+    uint8_t maxPorts; //eg a Strip can only have 1
+    uint16_t targetFrameRate = 40; // 0 = n/a
+    uint16_t throughput = 0; //us per byte or whatever, 0 = n/a
+    std::vector<TransformConfig> transforms; //adaptations to be applied directly on/right before RX/TX (eg strip layout shite)
+  };
+  struct InputConfig: public IOConfig {}; //anything specific?
+  struct OutputConfig: public IOConfig {
+    uint16_t powerDraw; //mA per field?
+    uint16_t powerCap; //watts per unit?
+  };
+  std::vector<InputConfig> inputs;
+  std::vector<OutputConfig> outputs;
+
+  struct PatchConfig {
+    // Something* from; //nah that's for the Patch, not its cfg
+    Source from; //Source is Port or w/e, or an intermediate pool buffer config
+    std::vector<TransformConfig> transforms;
+    Target to; //wait so is Target. how mirror RenderStage here...
+  };
+  std::vector<PatchConfig> patches;
+};
+PixtolConfig pixtolCfg("pixTol");
+// PixtolConfig::InputConfig art = PixtolConfig::InputConfig("ArtNet", 1, 512);
+PixtolConfig::InputConfig art("ArtNet", 1, 512);
+art.addPort("Universe 10", 10).addPort("Universe 11", 11);
+pixtolCfg.inputs.push_back(art); //emplace_back instead maybe
+
+{
+  "id": "Module",
+  "fieldType": "BYTE",
+  "fieldSize": 1,
+  "fieldCount": 1,
+  "types": [{
+    "id": "inputters",
+      "types": [{
+        "id": "NetworkIn", //NetworkBuffer maybe better name? DmxDerived?
+        "fieldCount": 512,
+        "targetframerate": 40,
+        "types": [
+          {"id": "ArtNet", "maxPorts": 4},
+          {"id": "sACN", "maxPorts": 32}, //correct? think read...
+          {"id": "OPC"}]
+   }, {
+     "id": "MQTT",
+     "fieldType": "FLOAT"
+   }, {
+     "id": "Sensor",
+     "fieldType": "FLOAT",
+     "types": [
+       {"id": "Voltage"}, {"id": "Fluid"}, {"id": "Air"},
+       {"id": "Beacon"}, {"id": "Signal Strength"}]
+   }]},
+     {"id": "outputters",
+      "types": [{
+        "id": "Strip",
+        "types": [{
+          "id": "SK6812",
+          "fieldSize": 4, //default RGBW, but also available RGB, single color etc...
+          "throughput": 10,
+          "types": [{
+            "id": "RGBW",
+            "types": [
+              {"id": "1m144 125", "fieldCount": 125},
+              {"id": "1m60 120", "fieldCount": 120 }]
+          }]
+          }. {
+            "id": "WS2812b",
+            "fieldSize": 3,
+            "types": [
+            {"id": "1m144 144", "fieldCount": 144},
+            {"id": "1m60 150", "fieldCount": 150 }]
+          }]
+      }, {
+     "id": "Pin"
+   }]},
+     {"id": "modulators",
+      "types": [{ //dunno name
+        "id": "dimmer",
+        "response": "InOut" //["attack", "release"]
+        }, { //or base "gain", subtype "dimmer" which only goes down. cause need up-gain too, and so similar...
+        "id": "Strobe", //or like Shutter, use of which is a strobe, when, targeting dimmer, (cant just use dimmer itself, since not abs)
+        // and strobe effect itself just basic generator that should be patchable onto anything
+        }, {
+        "id": "rotate", //buffer
+      }]},
+     {"id": "easing",
+      "kinds": [{"id": "None"}, {"id": "In"}, {"id": "Out"}, {"id": "InOut"}], //ehh yeah figure out
+      "types": [
+       {"id": "Cubic",
+         "functions": ["in-formula", "out-formula", "in-out-formula"],
+         {"id": "JoefancyMaz",
+         "functions": ["one", "2", "3"]}}]
+     }, {
+   "id": "generators", //animations etc
+   "types": [{
+     "id": "stagedAnimation", //or well this is only 1-param anim hehu
+     "stages" [{
+       "id": "animationStage", //eh name
+       "maxDuration": 0,
+       "minDuration": 0,
+       "maxFraction": 0,
+       "onComplete": "NEXT",
+
+       "actions": ["None"], //better name?
+
+       "types": [ //actual default implementations
+       {"id": "Pre", //debouncer stage, never repeats
+         "minDuration": 50 //rest auto, so this becomes exactly duration...
+       }, {
+         "id": "Lock", //so like, OPEN and CLOSED in shutter's stages
+         "actions": ["Level"] //"setAndLockLevel"
+         // impl cfg then has actual target... scaler?
+       }, {
+         "id": "Fade",
+         "actions": ["Level", "Saturation"], //think yellowing strobe ye
+         "bounds": [ 0.0, 1.0 ], //just for default...
+         "easing": "None" //for main anim progression/time response. actions surely have their own...
+         }],
+       }]
+   }, {
+     "id": "Fill", //put shit inna buffer. Prob a color
+     "action": "RepeatField",
+   }]
+   }]
+}
+
+// { //wip actual end-user cfg
+//   "type": "pixTol Stripper",
+//   "id": "pixTol-1",
+//   "inputs": [{
+//     "id": "ArtNet", //should merge with base cfg with defaults for id, so only need setting ports, optionally framerate...
+//     "ports": [{ "id": "Universe 10", "port": "10"}],
+//     "targetframerate": 40,
+//   }, {
+//     "id": "sACN",
+//     "ports": [{ "id": "Universe 10", "port": "10"}],
+//     "targetframerate": 40,
+//   }], //and so on, opc mqtt whatevs
+//   "outputs": [{
+//     "id": "SK6812", //again, look up against further cfg with defaults (so no need set fieldType, maxPorts... + 4 as default for fieldCount, etc)
+//     "fieldCount": 125,
+//     "ports": [{ "id": "Strip 1", "port": "RX"}],
+//     "throughput": 40, //us. but add stop pause overhead etc hmm...
+//     "transforms": [{ "mirror": false, "fold": false, "flip": true }]
+//   }]
+// }
+//
+// PixtolConfig::OutputConfig strip = PixtolConfig::OutputConfig("SK6812", 4, 125);
+// art.addPort("Strip 1", RX); //should then handle method automatically depending on what's available
+// // "RX", "D4" etc should obviously be abstracted, incl on hardware.
+// pixtolCfg.inputs.push_back(strip);
