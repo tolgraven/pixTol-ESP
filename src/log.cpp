aaf49bce19f0651de2ef16cb50626e1f8f15bcfd
Joen Tolgraven <joen.tolgraven@gmail.com>
2019-01-08 07:27:55 +0300

    Revert "wankery prepping log for future yet-further output methods..."
    This reverts commit 14e7abc11ea4075672d8c6519c3c15053c658d9a.

--- src/log.cpp
+++ src/log.cpp
@@ -1,102 +1,63 @@
 #include "log.h"
 
-// terminal.clear_screen();
-// terminal.hide_cursor();
-// terminal.set_display_style(ANSITerm::SGR_NONE);
-// terminal.set_text_colour(ANSITerm::SGR_RED);
-// // terminal.draw_box(3,3,19,5,ANSITerm::simple,false);
-// terminal.set_text_colour(ANSITerm::SGR_WHITE);
-// terminal.set_display_style(ANSITerm::SGR_BOLD);
-// terminal.set_cursor_position(5,4);
-// terminal.printf("ANSI Terminal");
-
-// void Log::log(const String& location, const Level level, const String& text) const {
-//   if(!shouldLog(level)) return;
-//   String mqtt_path(convert(leve l));
-//   mqtt_path.concat('/');
-//   mqtt_path.concat(location);
-//   if(mqtt   && Homie.isConnected())  setProperty(mqtt_path).setRetained(false).send(text);
-//   if(serial || !Homie.isConnected()) Serial.printf("%d: %s:%s\n", millis(), mqtt_path.c_str(), text.c_str());
-// }
-// should be bool i think
-
-// iLog::lvlStr[CRITICAL+1] = {"DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"};
-
-void iLog::log(const String& text, const Level level, const String& location) const {
-  if(!shouldLog(level)) return;
-  String path = convert(level) + ' ' + location;
-  // then ideally like for(auto& out: outputs) out.print(); and if disabled well, wont
-  if(outputs.at("Serial"))  //|| !Homie.isConnected()) //bad idea having connect check, if we've turned off we dont want yo
-    Serial.printf("%d  %s  %s", millis(), path.c_str(), text.c_str());
-}
-void iLog::ln(const String& text, const Level level, const String& location) const {
-  log(text + "\n", level, location);
-}
-void iLog::dbg(const String& text, const String& location) const {
-  log(text, DEBUG, location);
-}
-void iLog::err(const String& text, const String& location) const {
-  log(text, ERROR, location);
-}
-
-void iLog::f(const String& location, const Level level, const char *format, ...) const {
-  if(!shouldLog(level)) return;
-  va_list arg; va_start(arg, format);
-  char b[100]; //so, if too long, then what?
-  size_t len = vsnprintf(b, sizeof(b), format, arg); // just gets trunc presumably?
-  va_end(arg);
-  log(b, level, location);
-}
-//deprecated - use f()
-void iLog::logf(const String& location, const Level level, const char *format, ...) const {
-  if(!shouldLog(level)) return;
-  va_list arg; va_start(arg, format);
-  char b[100];
-  size_t len = vsnprintf(b, sizeof(b), format, arg);
-  va_end(arg);
-  log(b, level, location);
-}
-
-void iLog::fEvery(uint16_t numCalls, const String& id, const String& location, const Level level, const char *format, ...) const {
-  if(!shouldLog(level)) return;
-
-  static std::map<String, uint16_t> callTracker;
-  callTracker[id] = (callTracker.find(id) == callTracker.end())? 1: ++callTracker[id]; //init or inc
-  if(callTracker[id] < numCalls) return;
-  else callTracker.erase(id); //dunno when will be needed next so might as well remove, rather than zero
-
-  // f(location, level, format, arg); //no work right? :(
-  va_list arg; va_start(arg, format);
-  char b[100];
-  size_t len = vsnprintf(b, sizeof(b), format, arg);
-  va_end(arg);
-  log(location, level, b);
-}
-
-
 bool Log::handleInput(const String& property, const HomieRange& range, const String& value) {
-  f("Log settings", DEBUG, "requested setting property %s to %s", property.c_str(), value.c_str());
+  logf("Log settings", DEBUG, "requested setting property %s to %s", property.c_str(), value.c_str());
   if(property == "Level") {
     Level newLevel = convert(value);
     if(newLevel == INVALID) {
-      f("Log settings", ERROR, "Received invalid level %s.", value.c_str());
+      logf("Log settings", WARNING, "Received invalid level %s.", value.c_str());
       return false;
     }
     _level = newLevel;
-    f("Log settings", INFO, "Level now %s", value.c_str()); //this needed tho when setproperty does something right?
+    logf("Log settings", INFO, "Level now %s", value.c_str());
     setProperty("Level").send(value);
     return true;
-  } else if(property == "Serial" || property == "MQTT") { //check keys rather, ugh
-    bool state = (value.equalsIgnoreCase("on") || value == "1"); //or some lib fn for checken positive hapy words or signals heh
-    f("Log settings", INFO, "%s output %s", property.c_str(), state? "ON": "OFF");
-    setProperty(property).send(state? "On": "Off");
-    outputs[property] = state;
-    // if(property == "Serial") serial = state;
-    // if(property == "MQTT") mqtt = state;
+  } else if(property == "Serial") {
+    bool on = (value == "ON");
+    serial = on;
+    logf("Log settings", INFO, "Serial output %s", on? "on": "off");
+    setProperty("Serial").send(on? "On": "Off");
     return true;
   }
-  f("Log settings", ERROR, "Invalid: %s / %s", property.c_str(), value.c_str());
+  logf("Log settings", ERROR, "Invalid property %s, value %s", property.c_str(), value.c_str());
   return false;
+}
+
+void Log::log(const String& location, const Level level, const String& text) const {
+  if(!shouldLog(level)) return;
+  String mqtt_path(convert(level));
+  mqtt_path.concat('/');
+  mqtt_path.concat(location);
+  if(mqtt   && Homie.isConnected())  setProperty(mqtt_path).setRetained(false).send(text);
+  if(serial || !Homie.isConnected()) Serial.printf("%d: %s:%s\n", millis(), mqtt_path.c_str(), text.c_str());
+}
+
+void Log::logf(const String& location, const Level level, const char *format, ...) const {
+  if(!shouldLog(level)) return;
+  va_list arg;
+  va_start(arg, format);
+  char temp[100]; // char* buffer = temp;
+  size_t len = vsnprintf(temp, sizeof(temp), format, arg);
+  va_end(arg);
+  log(location, level, temp);
+}
+
+void Log::logfEvery(uint16_t numCalls, const String& id, const String& location, const Level level, const char *format, ...) const {
+  if(!shouldLog(level)) return;
+
+  static std::map<String, uint16_t> callTracker;
+  callTracker[id] = (callTracker.find(id) != callTracker.end())? callTracker++: 1;
+  // if(callTracker.find(id) != callTracker.end()) callTracker[id]++;
+  // else callTracker[id] = 1;
+  if(callTracker[id] < numCalls) return;
+  else callTracker.erase(id); //dunno when will be needed next so might as well remove, rather than zero
+
+  va_list arg;
+  va_start(arg, format);
+  char temp[100]; // char* buffer = temp;
+  size_t len = vsnprintf(temp, sizeof(temp), format, arg);
+  va_end(arg);
+  log(location, level, temp);
 }
 
 Log lg;
--- src/log.h
+++ src/log.h
@@ -2,90 +2,44 @@
 
 #include <map>
 #include <Homie.h>
-// #include <ANSITerm.h> //not an arduino lib. But def wanna extend shit with ansi oi
 
 #define __pfunc__ __PRETTY_FUNCTION__
-//there's also:
-// __builtin_FUNCTION()
-// __func__   obviously
 #define _DEBUG_ __func__, Log::DEBUG
 
-
 // #ifndef UNIT_TESTING //something like that, redefine logger to simple cout for native testing
-// class Log: public HomieNode { //prob have basic log then a sub from that with HomieNode, to decouple...
-class iLog { //prob have basic log then a sub from that with HomieNode, to decouple...
+class Log: public HomieNode {
 public:
-  iLog() {
-    // outputs["serial"] = Serial.available(); //I guess? maybe?
-    outputs["serial"] = true;
-    // if(Serial.available())
+  Log(): serial(true), mqtt(true), HomieNode("Log", "Logger") {
+    for(auto s: {"Level", "Serial", "MQTT"}) advertise(s).settable();
   }
-  // Log(): HomieNode("Log", "Logger") {
-  //   for(auto s: {"Level", "Serial", "MQTT"}) advertise(s).settable();
-  // }
-  // XXX is having enum and converting blabla worth it just to go DEBUG not "DEBUG"? heh
 	enum Level: int8_t { INVALID = -1, DEBUG = 0, INFO, WARNING, ERROR, CRITICAL }; //add an "NONE" at end to disable output fully?
-  // // XXX decouple from Homie - inherit a base Log class to HomieLog or something...
-	// bool handleInput(const String& property, const HomieRange& range, const String& value) override;
+  // XXX decouple from Homie - inherit a base Log class to HomieLog or something...
+	bool handleInput(const String& property, const HomieRange& range, const String& value) override;
 
-	// void log(const String& location, const Level level, const String& text) const;
-	// void log(const String& text, const Level level = INFO, const String& location = __PRETTY_FUNCTION__) const;
-	virtual void log(const String& text, const Level level = INFO, const String& location = __func__) const;
-  void ln(const String& text, const Level level, const String& location) const;
-  void dbg(const String& text, const String& location = __func__) const;
-  void err(const String& text, const String& location = __func__) const;
-
-	void f(const String& location, const Level level, const char *format, ...) const;
+	void log(const String& location, const Level level, const String& text) const;
 	void logf(const String& location, const Level level, const char *format, ...) const;
-	void fEvery(uint16_t numCalls, const String& id, const String& location, const Level level, const char *format, ...) const;
+	void logfEvery(uint16_t numCalls, const String& id, const String& location, const Level level, const char *format, ...) const;
 
 	bool shouldLog(Level lvl) const { return ((uint8_t)lvl >= (uint8_t)_level); }
 	void setLevel(Level lvl) { if(lvl >= DEBUG && lvl <= CRITICAL) _level = lvl; }
-  void setOption(const String& option, const String& value); //use in future
 
-protected:
+private:
+  static const Level defaultLevel = DEBUG;
 	Level _level;
-  std::map<String, bool> outputs; // = {{"Serial", true}};
-	// bool serial = true; //, mqtt = true;
-  // std::map<String, bool> outputs; //more like, i guess. like later will have screen, web...
-  // or maybe logger only registers and further ups keep track on where to send text...
-  String lvlStr[CRITICAL+1] = {"DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"};
-  // static String lvlStr[CRITICAL+1];
+	bool serial;
+  bool mqtt;
 
-	Level convert(const String& lvl) const {
-	// Level convert(String& lvl) {
-    // static String lvlStr[CRITICAL+1] = {"DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"};
-    for(uint8_t ilvl = DEBUG; ilvl <= CRITICAL; ilvl++)
-      if(lvl.equalsIgnoreCase(lvlStr[ilvl])) return static_cast<Level>(ilvl);
+	static Level convert(const String& lvl) {
+    String lvlStr[CRITICAL+1] = {"DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"};
+    for(uint8_t iLevel = DEBUG; iLevel <= CRITICAL; iLevel++)
+      if(lvl.equalsIgnoreCase(lvlStr[iLevel])) return static_cast<Level>(iLevel);
     return INVALID;
   }
-	String convert(const Level lvl) const {
-	// String convert(Level lvl) {
-    // static String lvlStr[CRITICAL+1] = {"DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"};
+	static String convert(const Level lvl) {
+    String lvlStr[CRITICAL+1] = {"DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"};
     auto index = constrain(lvl, INVALID, CRITICAL+1);
     if(index >= 0) return lvlStr[index];
     else return "INVALID";
-  }
-};
-
-//yeah this is dumb. use the attach Outputters idea instead and just reg HomieNode, dont be one...
-class Log: public iLog, public HomieNode {
-  public:
-  Log(): iLog(), HomieNode("Log", "Logger") {
-    outputs["MQTT"] = true;
-    for(auto s: {"Level", "Serial", "MQTT"}) advertise(s).settable();
-    // for(auto s: {"Level", outputs.}) advertise(s).settable(); //ugly lol useless fix
-  }
-  // bool mqtt = true;
-  // bool tryProperty(const String& property);
-	bool handleInput(const String& property, const HomieRange& range, const String& value) override;
-
-	void log(const String& text, const Level level = INFO, const String& location = __func__) const override {
-    if(!shouldLog(level)) return;
-    iLog::log(text, level, location);
-    String path = convert(level) + ' ' + location;
-    if(outputs.at("MQTT")  && Homie.isConnected())
-      setProperty(path).setRetained(false).send(text);
   }
 };
 
