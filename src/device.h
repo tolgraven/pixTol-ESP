2f69b9758dbd5e36fc22ddebd62399035e0197c8
Joen Tolgraven <joen.tolgraven@gmail.com>
2020-03-01 08:00:59 +0100

    break out wifi stuff to own file, polish still very messy Device

--- src/device.h
+++ src/device.h
@@ -1,18 +1,19 @@
 // firmware, updates, wifi detail swarm sharing stuff...
 #pragma once
 
-#include <Arduino.h> //needed by linters
-/* #include <Homie.h> */
+#include <vector>
 
-/* #include <WifiEspNowBroadcast.h> */
-/* #include <esp_now.h> */
+#include <FS.h>                   //this needs to be first, or it all crashes and burns...
+#include <Ticker.h>
+// #include <NtpClientLib.h>
 
-/* #include "config.h" */
-/* #include "io/unit.h" */
 #include "log.h"
 #include "firmware.h"
 #include "watchdog.h"
 #include "debuglog.h"
+#include "event.h"
+#include "io/networking.h"
+
 
 enum class Status { //based off homie, wrap that first and extend with eg RDM, OPC, HomeKit etc status events
   Invalid = -1, Setup = 1, Normal,
@@ -22,55 +23,126 @@ enum class Status { //based off homie, wrap that first and extend with eg RDM, O
   PrepareReset, PrepareSleep
 };
 
-class Device { //inits hardware stuff, holds Updater, PhysicalUI/components, more?
-private:
+enum class DeviceStatus { //based off homie, wrap that first and extend with eg RDM, OPC, HomeKit etc status events
+  Invalid = -1, Boot = 1, Normal, Config, FirmwareUpdate, // then subevt OtaStart, OtaTick, OtaSuccess, OtaFailure,
+  PrepareRestart, PrepareSleep
+};
+enum class EventType { // based on OTA stuff but could be used for lots of stuff?
+  // Invalid = -1, Start = 1, Progress, Success, Error, Custom, Unknown
+  Invalid = -1, Start = 1, Progress, End, Custom, Unknown
+};
+enum class EventResult {
+  Invalid = -1, Success = 0, Error // and then error codes all the way down?
+};
+struct BasicEvent {
+  DeviceStatus location;
+  EventType type;
+  EventResult error;
+  uint32_t time; // also want NTP actual time I guess. tho RTC drift and blabla
+};
+// urr anyways mod so basically like homies events, except scheduled not interrupt.
+// so events arent handled until next pass through main loop
+//
+// then again no reason to push too far that way and not migrate to 32 and actual threads&scheduling lol
+// see eg ping goes bananas when letting Artnet out go full tilt...
+//
+
+// while really a million ways for signals and tho resolution is lol, midi is still the king of easy control input heh.
+// also just for generating, tap a synth. record shit really all my early really exciting "fuck building just repurpose" ideas just...  fell off.
+// I was hooking Live up to my fucking modded milights five years ago through fucking node-red and it worked fuckng badass!!
+// ffs take all this knowledge and build some practical and actually usable shit.  show it to people.
+class Device { //inits hardware stuff, holds Updaters, PhysicalUI/components, more?
+  private:
   String _id;
   std::map<String, Updater*> updater;
-  // LoopWatchdog* lwd = nullptr;
-  /* PhysicalUI* ui = nullptr; */
-  // WebUI* webUI;
+  // NTPTime time; // reckon will help with the rougher (simpler) parts of sync https://github.com/aharshac/EasyNTPClient
+  /* PhysicalUI* ui = nullptr; */ // WebUI* webUI;
   Status lastStatus;
-public:
+  Ticker recurring;
+  std::unique_ptr<WifiConnection> wifi;
+  // FileIO fs;
+  String cfgFile = "/config.json"; //let's just use WifiManager quick while then move to ConfigManager
+  public:
 
   Debug* debug = nullptr;
   uint32_t timeBootDone, timeCurrent;
   /* Config* config; //we'll need to make one of these. */
 
-  Device() { // serial, random seed mean this before anything else but that makes sense since want watchdog, statusled, display etc from boot...
+  Device(const String& id): _id(id) { // serial, random seed mean this before anything else but that makes sense since want watchdog, statusled, display etc from boot...
     uint8_t stackStart = 0;
     debug = new Debug(&stackStart);
     Serial.begin(SERIAL_BAUD); //well, not if uart strip tho...
-    lg.initOutput("Serial");
+    lg.initOutput("Serial");  //logging can be done after here
+
+    uint32_t finishAt = millis() + 2000;
+    auto dotter = [finishAt]() {
+                    if(millis() < finishAt) {
+                      Serial.print("."); return true;
+                    } else return false;
+                  };
+    recurring.attach_ms(250, dotter); // so, cant find where in lib return false to detach actually handled?
+
+    lwd.init(); //muy important, chilled out constructor
+    lg.dbg(lwd.getResetReason() + (lwd.softResettable()? ", softResettable!": ""));
+    // lg.dbg(debug->getStacktrace());
+    //
+    // OTA update leads to crash in Neo i2s_slc_isr
+    // SaveCrash leads to stack overflow repeat exception while handling exception
+    // debug->saveCrash.print(lg); //can use our logger as output. i thought?
+    // debug->saveCrash.print(); //can use our logger as output. i thought?
+    // if(debug->saveCrash.count() >= 3) debug->saveCrash.clear();
+    debug->stackAvailableLog();
+
     initRNG();
+    wifi = std::unique_ptr<WifiConnection>(new WifiConnection("pixTol-proto", "wifi"));
+    wifi->start(); //blocking...
 
     updater["OTA"] = new ArduinoOTAUpdater(_id);
     /* updater["HTTP"] = new HttpUpdater(String(Homie.getConfiguration().mqtt.server.host), 1880, s); */
-    // EEPROM.begin(512); SPIFFS.begin();
-    // lg.dbg("Done Device");
-  }
-  ~Device() {
-    updater.clear();
-  }
-  void finalizeBoot() { // log time etc... what else?
+
+    handleBootLoop();
+
     /* lwd.stamp(PIXTOL_SETUP_EXIT); */
     timeBootDone = micros();
-    lg.dbg("Reset reason: " + lwd.getResetReason() + ", softResettable? "
-        + (lwd.softResettable()? "YES": "NOT"));
-    lg.ln("Setup complete, handing over to main loop...\n", Log::INFO, "Boot");
+    debug->stackAvailableLog();
+
+    lg.dbg("Done Device");
   }
 
-  bool writeFile();
-  void readFile();
-  bool writeRTC();
-  void readRTC();
-  virtual void platformSpecific() {
-    /* lg.f(__func__, Log::INFO, "Not implemented. Here go esp32/66/whatever specific things."); */
-  } // export $PIOPLATFORM from .ini as PLATFORM_NAME
+  ~Device() { updater.clear(); }
+
+  void handleBootLoop() {
+    auto resetCount = lwd.getCount();
+    if(resetCount >= 3 && resetCount % 2 > 0) { // this also when we can take the time to print the traces I guess, and clear...
+      lg.dbg("\n\nSAVE YOURSELF\n\n");
+      // debug->saveCrash.print();
+      // debug->saveCrash.clear();
+
+      // disable helpfully actual culprit but main is just ensure updater lives
+      // would still make sense just stalling here tho still feeding wdt giving chance to update...
+      uint32_t otaTimeout = constrain(resetCount * 3000, 3000, 30000);
+      uint32_t finishAt = millis() + otaTimeout;
+      auto dotter = [finishAt]() {
+                      if(millis() < finishAt) {
+                        Serial.print("."); return true;
+                      } else return false; };
+      // recurring.attach_scheduled(1, dotter); // oh yeah bc we never reach loop, no shit.
+      recurring.attach(1, dotter); // so, cant find where in lib return false to detach actually handled?
+      // nor where I read about it. hmm.
+      while(millis() < finishAt) {
+        lwd.feed();
+        this->loop();
+        lwd.stamp();
+#ifdef ESP8266
+        ESP.wdtFeed();
+#endif
+      }
+    }
+  }
+
 
   void loop() {
-    timeCurrent = micros();
     for(auto& up: updater) up.second->loop(); //would fit better here than in "IOT" (shit name) anyways?
-    // lwd.feed(); //if so device loop must go first in loop
   }
 
   void initRNG() { // slightly less shit-resolution analogRead random seed ting...
@@ -82,32 +154,9 @@ public:
     } randomSeed(seed);
   }
 
-  void tryWifi() {
-    static uint8_t maxAPs = 5;
-    /* bool goStation = false; */
-    bool connected = false;
-    wifi_station_ap_number_set(maxAPs); //max number of saved APs
-    struct station_config config[maxAPs];
-    wifi_station_get_ap_info(config);
-
-    uint8_t lastId = wifi_station_get_current_ap_id(); //which index
-    // on connect, then save to spiffs? but hopefully works disconnected, sop can just go down index...
-    while(!connected) {
-      lastId++;
-      if(lastId >= maxAPs) {
-        //first check spiffs for further shit
-        /* goStation = true; */
-        break;
-      }
-      wifi_station_ap_change(lastId);
-      // try connection etc
-      // inbetween each, also look for (hardcoded?) mesh leader station, whether it's got news
-    }
-  }
-  void restart() {
-    //should do it good way, through lwd etc...
-  }
 };
+
+
 
 class SystemComponent {
   public:
@@ -116,33 +165,75 @@ class SystemComponent {
 };
 
 #define ESP_NOW_MAX_PEERS 20
-/* class Swarm: public SystemComponent { */
-/*   public: */
-/*   bool master = false; // should be possible for any device to be master once it gets creds */
-/*   Swarm() { */
-/*     // should be created dynamically when not getting online and yada. Or is online + requested */
-/*     // then register self ya? */
-/*     // mod WifiEspNow to not have global */
 
-/*   } */
-/*   static void onPacket(const uint8_t mac[6], const uint8_t* buf, size_t count, void* cbarg) { */
+class PowerBudget {
+  // had some minor power stuff in the battery experiment but so many aspects.
+  // wearable/battery etc can limit to target much better longetivity,
+  // but smaller stuff (USB etc!) also have massive peak power budget
+  // and working with that can run p loads of stuff off little (+ even better with bunch big caps???)
+  // just by generally dim and high lum v local and transient.
+  //
+  // Question is how you enforce that in a nice lookng way.
+  // But basic thing (fastled does it?) we know about how much each LED uses and what is active...
+  //
+  // so put one in an Outputter run the numbers adding up each frames total.
+  // and just ev run into compression/deminishing returns.
+  //
+  // more effect wise remember old ideas for afterglow with slow compressor making peaks stand out etc
+};
+class DataIO: public Named { // something common then subclass for SPIFFS, EEPROM, RTC... different use cases
+  public:
+  DataIO(): Named() {} // or duh use c++ ffs you dummy. fstream...
+  virtual ~DataIO() {}
 
-/*   } */
-/*   void broadcastWifiCredentials() { //look up the encryption thing. but will obviously need some kind of pre-shared secret.. */
-/*     WifiEspNowPeerInfo peers[ESP_NOW_MAX_PEERS]; */
-/*     int nPeers = std::min(WifiEspNow.listPeers(peers, ESP_NOW_MAX_PEERS), ESP_NOW_MAX_PEERS); */
-/*     for (int i = 0; i < nPeers; ++i) { */
-/*       Serial.printf(" %02X:%02X:%02X:%02X:%02X:%02X", peers[i].mac[0], peers[i].mac[1], peers[i].mac[2], peers[i].mac[3], peers[i].mac[4], peers[i].mac[5]); */
-/*     } */
-/*   } */
+  virtual bool start() = 0;
+  virtual bool stop() = 0;
+  virtual bool clear() = 0;
+  virtual bool write(const String& location, const String& contents) = 0;
+  virtual std::unique_ptr<char[]> read(const String& location) = 0; // virtual char* read() = 0;
+  // bool writeConfig(); // SPIFFS, RTC, EEPROM..
+  // void readConfig();
+};
+// class FileIO: public DataIO {
+//   public:
+//   FileIO(): DataIO() {}
+//   virtual ~FileIO() { stop(); }
 
-/*   void loop() { */
-/*   } */
-/* }; */
-// SWARM STUFF:
-//  - When can't connect wifi on fresh boot, periodically
-//  * turn on AP, mqtt server, http server storing known good fw & config (for leader)
-//  * attempt connect to leader AP / mesh BLE (on ESP32 later) and watch for Homie broadcasts of new wifi details.
-//  And/or if plugged serial DMX, RDM route, other 2.4GHz protocol then check that, etc...
-// turns out there's already something for it!
-//
+//   virtual bool start() { return SPIFFS.begin(); } // or even skip theses and enough w ctor/dtor...
+//   virtual bool stop() { SPIFFS.end(); return true; }
+//   virtual bool clear() { return SPIFFS.format(); }
+//   virtual bool write(const String& path, const String& contents) {
+//     File file = SPIFFS.open(path, "w"); //whether or not file exists we can write to it...
+//     if(!file) { return false; }
+//     file.print(contents);
+//     file.close();
+//     return true;
+//   }
+
+//   // virtual char* read(const String& path) {
+//   virtual std::unique_ptr<char[]> read(const String& path) {
+//     if(!SPIFFS.exists(path)) return nullptr;
+//     File file = SPIFFS.open(path, "r"); //file exists, reading and loading
+//     if(!file)  return nullptr;  // for long like these maybe a helper fn that logs an error and returns... or statuscodes.
+
+//     size_t size = file.size();
+    // std::unique_ptr<char[]> buf(new char[size]); // Allocate a buffer to store contents of the file.
+    // file.readBytes(buf.get(), size);
+
+    // return buf;
+  // }
+// };
+// class EepromIO: public DataIO { //raw flash writes tho like, eh, need?
+//   size_t size;
+//   public:
+//   EepromIO(size_t size = 512): DataIO(), size(size) { }
+//   virtual ~EepromIO() { stop(); }
+//   virtual bool start() { EEPROM.begin(size); }
+//   virtual bool stop() { EEPROM.end(); }
+//   virtual bool write(const String& fileName, const String& contents) { }
+//   virtual std::unique_ptr<char[]> read(const String& fileName) { }
+// };
+// class DataFormat { //to encode as json bubt also binary packing crap yada
+//   DataFormat() {}
+//   ~DataFormat() {}
+// };
---
+++ src/io/networking.h
@@ -0,0 +1,188 @@
+#pragma once
+
+// #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+// #include <AsyncMqttClient.h>
+#if defined(ESP8266)
+#include <ESP8266mDNS.h>
+#elif defined(ESP32)
+#include <mDNS.h>
+#endif
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+// #include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
+
+// #include <cont.h> //somethong about continuations, might come handy
+/* #include <WifiEspNowBroadcast.h> */
+/* #include <esp_now.h> */
+
+
+struct MDNSService {
+  String name, protocol; //or just enum lol
+  uint16_t port;
+  struct InfoField { String name, value; };
+  std::vector<InfoField> infoFields;
+};
+
+class NetworkConnection: public Named { // wifi + mesh/Now, bt, anything else ad-hoc? or generic enough for MQTT etc? tho diff cat
+  public:
+  NetworkConnection(const String& id, const String& type):
+    Named(id, type) {}
+  virtual ~NetworkConnection() {}
+
+  virtual bool start() = 0;
+  virtual void stop() = 0;
+  virtual bool isConnected() = 0;
+  virtual bool resetConfiguration() = 0;
+  virtual void onConfigMode() {}
+  virtual void onConfigured() {}
+  virtual void onConnected() {}
+  virtual void onDisconnected() {}
+};
+// afa wrapping wifimanager think support for pre-set creds and auto or no fallback, manual cfg reset and conf AP...
+class WifiConnection: public NetworkConnection {
+  std::unique_ptr<WiFiManager> wifiManager;
+  std::vector<WiFiManagerParameter> customParams; // After connecting, parameter.getValue() will get you the configured value id/name placeholder/prompt default length
+
+  String hotspotDefaultPassword = "baconmanna";
+
+  String mqttServer = "paj";
+  String universe = "2";
+  public:
+  WifiConnection(const String& id, const String& type):
+   NetworkConnection(id, type) { //provide some kinda (likely subset of) config obj...
+
+    wifiManager = std::unique_ptr<WiFiManager>(new WiFiManager); //unlikely this obj itself will be needed for so long.
+    // not sure how best think about it. maybe delete end of start() but might want be able use without rebooting too
+
+#ifndef DEBUG
+    wifiManager->setDebugOutput(false); //silence
+#endif
+    // wifiManager->setSaveConfigCallback(saveConfigCallback); //can use std::bind etc? check
+    // wifiManager->setSTAStaticIPConfig(ips[NET], ips[GATEWAY], ips[MASK]); // need basic common conf for this typa...
+    // wifiManager->setAPStaticIPConfig(); // for portal...
+    wifiManager->setMinimumSignalQuality(20); //set minimu quality of signal so it ignores AP's under that quality defaults to 8%
+    wifiManager->setTimeout(300); //timeout til conf portal turns off, useful to make it all retry or go to sleep in seconds
+    // using namespace std::placeholders;
+    // wifiManager->setAPCallback(&WifiConnection::onConfigMode, this); // when fails to connect and launches hotspot
+
+    // customParams.emplace_back("server", "mqtt server", mqtt_server, 40); // eh these p clunky lol
+    // customParams.emplace_back("universe", "dmx", mqtt_port, 6);
+    for(auto&& param: customParams)
+      wifiManager->addParameter(&param);
+    //strcpy(mqtt_server, custom_mqtt_server.getValue());
+    //strcpy(mqtt_port, custom_mqtt_port.getValue());
+    // saveConfig(shouldSaveConfig); //read/save updated parameters
+    // wifiManager->setHostname(id().c_str());
+    // wifiManager->setHostname(id.c_str()); // uh causes garbage, weird...
+  }
+  virtual ~WifiConnection() { if(isConnected()) stop(); } // could nuke earlier, as soon done setting basic settings (then rest should be mqtt, RDM, own page or whatever)
+
+  bool start() override {
+   // IDEA WAS:
+   // first fire an event, start a timer periodically pushing msg we're in connec mode...
+   // but since we're in their context can't actually properly act on messages anyways
+   // so fork lib and can add poll-to-run also during connect,
+   // not actually deferring boot til connect, multiAP, etc...
+
+    wifiManager->setHostname(id().c_str());
+    // auto eventSender = fn;
+    if(!wifiManager->autoConnect(id().c_str(), hotspotDefaultPassword.c_str())) {
+      lg.err("Failed to connect to Wifi or start configuration hotspot..", __func__);
+      return false; // our caller will have to ensure light starts flashing red and shit asplode
+    }
+#ifdef ESP8266
+    String chipId = String(ESP.getChipId(), HEX);
+    MDNS.setInstanceName((id() + " (" + chipId + ")").c_str()); // printf("%s (%s)", );
+
+    if(MDNS.begin(id().c_str())) {
+      MDNSService services[2] = {{"mqtt",    "tcp", 1883},
+                                 {"art-net", "udp", 6564, {{"Manufacturer", "tolgrAVen"}}}}; // no go yet needs moar auto
+
+       for(auto svc: services) { //however not hapnening here. depends on ports/services in use. Prob they tasked sending to here.
+        MDNS.addService(svc.name, svc.protocol, svc.port);
+        for(auto& txt: svc.infoFields)
+        MDNS.addServiceTxt(svc.name, svc.protocol, txt.name, txt.value);
+       } // MDNS.addService("e131", "udp", E131_DEFAULT_PORT); MDNS.addService("http", "tcp", HTTP_PORT);
+       // MDNS.addServiceTxt("e131", "udp", "TxtVers", String(RDMNET_DNSSD_TXTVERS)); MDNS.addServiceTxt("e131", "udp", "ConfScope", RDMNET_DEFAULT_SCOPE); MDNS.addServiceTxt("e131", "udp", "E133Vers", String(RDMNET_DNSSD_E133VERS));
+      }
+#endif
+      // delete wifiManager;
+      return true;
+    }
+    virtual void stop() {
+     WiFi.disconnect(); // disconnect/sleep wifi. use esp fns directly
+    }
+    virtual bool resetConfiguration() {
+     wifiManager->resetSettings(); //reset settings - for testing
+     wifiManager->startConfigPortal(); // like autoConnect but manually...
+     return true; // I guess.
+    }
+    virtual bool isConnected() { return true; }
+    virtual void onConfigMode() { DEBUG("WIFI AP PSAWNED"); }
+    // inject custom head element You can use this to any html bit to the head of the configuration portal. If you add a <style> element, bare in mind it overwrites the included css, not replaces.
+    //    wifiManager.setCustomHeadElement("<style>html{filter: invert(100%); -webkit-filter: invert(100%);}</style>");
+    // inject a custom bit of html in the configuration form
+    //    WiFiManagerParameter custom_text("<p>This is just a text paragraph</p>");
+    //    wifiManager.addParameter(&custom_text);
+  };
+  // class BTConnection: public NetworkConnection {
+  //   BTConnection(): NetworkConnection() { }
+  //   virtual ~BTConnection() {}
+  // }; // and then some fancier overlays for swarm stuff ESPnow
+  // also semi related to inputter/outputter concept and how they can be semi medium agnostic
+  // so ex in some cases might want to use different kinds of 2.4Ghz or whatever
+
+
+  //void tryWifi() {
+  //  static uint8_t maxAPs = 5;
+  //  /* bool goStation = false; */
+  //  bool connected = false;
+  //  wifi_station_ap_number_set(maxAPs); //max number of saved APs
+  //  struct station_config config[maxAPs];
+  //  wifi_station_get_ap_info(config);
+
+  //  uint8_t lastId = wifi_station_get_current_ap_id(); //which index
+  //  // on connect, then save to spiffs? but hopefully works disconnected, sop can just go down index...
+  //  while(!connected) {
+  //    lastId++;
+  //    if(lastId >= maxAPs) {
+  //      //first check spiffs for further shit
+  //      /* goStation = true; */
+  //      break;
+  //    }
+  //    wifi_station_ap_change(lastId);
+  //    // try connection etc
+  //    // inbetween each, also look for (hardcoded?) mesh leader station, whether it's got news
+  //  }
+  //}
+
+/* class Swarm: public SystemComponent { */
+/*   public: */
+/*   bool master = false; // should be possible for any device to be master once it gets creds */
+/*   Swarm() { */
+/*     // should be created dynamically when not getting online and yada. Or is online + requested */
+/*     // then register self ya? */
+/*     // mod WifiEspNow to not have global */
+
+/*   } */
+/*   static void onPacket(const uint8_t mac[6], const uint8_t* buf, size_t count, void* cbarg) { */
+
+/*   } */
+/*   void broadcastWifiCredentials() { //look up the encryption thing. but will obviously need some kind of pre-shared secret.. */
+/*     WifiEspNowPeerInfo peers[ESP_NOW_MAX_PEERS]; */
+/*     int nPeers = std::min(WifiEspNow.listPeers(peers, ESP_NOW_MAX_PEERS), ESP_NOW_MAX_PEERS); */
+/*     for (int i = 0; i < nPeers; ++i) { */
+/*       Serial.printf(" %02X:%02X:%02X:%02X:%02X:%02X", peers[i].mac[0], peers[i].mac[1], peers[i].mac[2], peers[i].mac[3], peers[i].mac[4], peers[i].mac[5]); */
+/*     } */
+/*   } */
+
+/*   void loop() { */
+/*   } */
+/* }; */
+// SWARM STUFF:
+//  - When can't connect wifi on fresh boot, periodically
+//  * turn on AP, mqtt server, http server storing known good fw & config (for leader)
+//  * attempt connect to leader AP / mesh BLE (on ESP32 later) and watch for Homie broadcasts of new wifi details.
+//  And/or if plugged serial DMX, RDM route, other 2.4GHz protocol then check that, etc...
+// turns out there's already something for it!
+//
+//
