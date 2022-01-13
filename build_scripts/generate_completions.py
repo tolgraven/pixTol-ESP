752fcd47849a73970e5e73675a4a1a6ece39f498
Joen Tolgraven <joen.tolgraven@gmail.com>
2022-01-13 09:33:54 +0100

    cleanup esp8266/pio stuff

--- build_scripts/generate_completions.py
+++
@@ -1,6 +0,0 @@
-Import("env")
-
-def reload_compile_commands(source, target, env):
-    env.Execute("fish -c 'pio_compile_commands'")
-
-env.AddPreAction("upload", reload_compile_commands)
--- build_scripts/pre_serial_upload.py
+++
@@ -1,7 +0,0 @@
-Import("env")
-
-def kill_monitor(source, target, env):
-    # env.Execute("fish -c 'kill (lsof -t /dev/*usbserial*) >&- ^&-'")
-    env.Execute("pkill -f 'pio device monitor'")
-env.AddPreAction("upload", kill_monitor)
-env.AddPreAction("uploadfs", kill_monitor)
